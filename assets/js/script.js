290cab259375c3593f2b2afbe3e4485371541ebb
kabdomora <114969831+kabdomora@users.noreply.github.com>
2023-01-14 23:08:43 -0800

    dummy data added for quiz questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,3 +14,45 @@ startBtn.addEventListener("click", function() {
 });
 // hides start button and makes quiz question container visible
 
+let allQuestions = [
+  {
+    question: "Question 1",
+    options: ["1", "2", "3", "4"],
+    answer: "2",
+  }
+  {
+    question: "Question 2",
+    options: ["1", "2", "3", "4"],
+    answer: "1",
+  }
+  {
+    question: "Question 3",
+    options: ["1", "2", "3", "4"],
+    answer: "2",
+  }
+  {
+    question: "Question 4",
+    options: ["1", "2", "3", "4"],
+    answer: "4",
+  }
+  {
+    question: "Question 5",
+    options: ["1", "2", "3", "4"],
+    answer: "3",
+  }
+  {
+    question: "Question 6",
+    options: ["1", "2", "3", "4"],
+    answer: "1",
+  }
+  {
+    question: "Question 7",
+    options: ["1", "2", "3", "4"],
+    answer: "2",
+  }
+  {
+    question: "Question 8",
+    options: ["1", "2", "3", "4"],
+    answer: "3",
+  }
+]
