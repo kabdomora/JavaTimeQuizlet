7e963d99897511deaff9403a67390324ccebf26c
kabdomora <114969831+kabdomora@users.noreply.github.com>
2023-01-14 23:03:56 -0800

    elements linked for js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 var startBtn = document.querySelector("#start");
 var begin = document.querySelector(".card-start");
 var quiz = document.querySelector(".card");
+var timer = document.querySelector("#timer");
+var question = document.querySelector("#question");
+var choices = document.querySelector("#choices");
+var highScores = document.querySelector("#high-scores");
+
 
 
 startBtn.addEventListener("click", function() {
@@ -8,5 +13,4 @@ startBtn.addEventListener("click", function() {
 	quiz.setAttribute("style", "display:block");
 });
 // hides start button and makes quiz question container visible
-
 
